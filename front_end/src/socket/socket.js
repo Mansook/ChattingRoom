40d4e534648cabefa84f8e53a02499656d5f36fd
minseok <orgin20@naver.com>
2024-06-03 13:52:47 +0900

    ec2

--- front_end/src/socket/socket.js
+++ front_end/src/socket/socket.js
@@ -1,12 +1,11 @@
 import io from "socket.io-client";
 
-export const socket = io("localhost:8000");
+//export const socket = io("localhost:8000");
 
 
-/*export const socket = io("https://6f52-112-150-250-55.ngrok-free.app",{
+export const socket = io("ec2-43-201-46-225.ap-northeast-2.compute.amazonaws.com:8000",{
     withCredentials: true,
     extraHeaders: {
         "my-custom-header": "abcd"
     }
 });
-*/
