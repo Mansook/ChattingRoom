13eb337a2207e5d31c159d8b32628eb35cb1958a
minseok <orgin20@naver.com>
2024-06-08 22:57:57 +0900

    deployed url proxy

--- front_end/src/lib/api/client.js
+++ front_end/src/lib/api/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL:"https://f853-175-116-124-147.ngrok-free.app",
-    //baseURL: "https://port-0-filchatter-1fgm12klx5u6yb5.sel5.cloudtype.app",
+    //baseURL:"https://f853-175-116-124-147.ngrok-free.app",
+    baseURL: "https://port-0-filchatter-1fgm12klx5u6yb5.sel5.cloudtype.app",
 });
 
 export default client;
--- front_end/src/setupProxy.js
+++ front_end/src/setupProxy.js
@@ -4,11 +4,12 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: "https://f853-175-116-124-147.ngrok-free.app",
+      target: "https://port-0-filchatter-1fgm12klx5u6yb5.sel5.cloudtype.app",
       changeOrigin: true,
     })
   );
 };
+//"https://f853-175-116-124-147.ngrok-free.app"
 //""https://port-0-filchatter-1fgm12klx5u6yb5.sel5.cloudtype.app"
 //http://218.239.229.119:4440
 /*
