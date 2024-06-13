031f51ecdd23b5b0ccaac11125f92c20ea3c65c4
minseok <orgin20@naver.com>
2024-06-13 16:58:21 +0900

    styled deleted

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,6 @@
 import ChatContainer from "./container/ChatContainer";
-import ApiTest from "./container/ApiTest";
 import HomeContainer from "./container/HomeContainer";
 import { Routes, Route,Navigate } from "react-router-dom";
-import styled from 'styled-components';
 
 function App() {
   return (
